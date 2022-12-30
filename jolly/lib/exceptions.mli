a934b9da77c848ae0030d3c6a57f17342415e3e4
Sean Brynjolfsson <smb459@cornell.edu>
2022-12-29 21:11:15 -0500

    Write exception docs.

--- jolly/lib/exceptions.mli
+++ jolly/lib/exceptions.mli
@@ -1,6 +1,9 @@
 
 exception Length_mismatch of int
+(** [Length_mismatch diff] is thrown if the lengths of two lists differ by [diff] when they should be equal in length.*)
 
 exception Label_collision of string
+(** [Label_collision label] is thrown if a [label] is already reserved and an attempt is made to bind something to it.*)
 
 exception Label_not_found of string
+(** [Label_not_found label] is thrown if [label] is expected to exist and does not.*)
